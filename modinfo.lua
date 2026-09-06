862335ea7ace4bc0e039bd91507d016f25ce6ea2
nroj95 <236590761+nroj95@users.noreply.github.com>
2026-09-07 01:19:13 +0200

    wire mod icon

--- modinfo.lua
+++ modinfo.lua
@@ -23,6 +23,9 @@ reign_of_giants_compatible = false
 client_only_mod = false
 all_clients_require_mod = true
 
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
 server_filter_tags = {
     "sleep",
     "tent",
